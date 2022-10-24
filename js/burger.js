203919e575f7c79a406b9903e781883573ce4135
Valkiria0000 <{ID}+{Valkiria0000}@Valkiria0000.noreply.github.com>
2022-10-24 22:36:18 +0300

    add burger js

---
+++ js/burger.js
@@ -0,0 +1,15 @@
+function burger() {
+  const menu = document.querySelector(".navmenu");
+
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", function (e) {
+    if (this.closest(".burger")) {
+      menu.style = `
+     display: flex;
+     `;
+      menu.classList.toggle("menu_active");
+      burger.children[0].classList.toggle("burger-active");
+    }
+  });
+}
+burger();
